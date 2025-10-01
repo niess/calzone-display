73fe14d30f067c8b9957f9a383426df70cc6a0f1
Valentin Niess <niess@in2p3.fr>
2025-10-01 16:41:10 +0200

    Toggle stats

--- crates/display/src/ui/stats.rs
+++ crates/display/src/ui/stats.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
-use bevy::diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin};
+use bevy::diagnostic::{
+    DiagnosticsStore, FrameTimeDiagnosticsPlugin, SystemInformationDiagnosticsPlugin,
+};
 use core::time::Duration;
 use crate::app::AppState;
 use super::{UiRoot, UiText, UiWindow};
@@ -9,77 +11,57 @@ pub fn build(app: &mut App) {
     app
         .add_plugins((
             FrameTimeDiagnosticsPlugin::default(),
+            SystemInformationDiagnosticsPlugin
         ))
-        .add_systems(OnEnter(AppState::Display), setup_panel)
-        .add_systems(Update,
+        .add_systems(Startup, initialise)
+        .add_systems(OnEnter(AppState::Display),
+            setup_panel
+                .run_if(in_state(StatsState::Enabled))
+        )
+        .add_systems(Update, (
             update_text
                 .run_if(in_state(AppState::Display))
-        );
+                .run_if(in_state(StatsState::Enabled)),
+            toggle_stats
+                .run_if(in_state(AppState::Display)),
+        ))
+        .init_state::<StatsState>();
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States)]
+enum StatsState {
+    #[default]
+    Disabled,
+    Enabled,
 }
 
 #[derive(Component)]
 enum Property {
+    Cpu,
     Fps,
+    Memory,
+}
+
+#[derive(Component)]
+struct StatsPanel;
+
+fn initialise (
+    mut store: ResMut<DiagnosticsStore>,
+) {
+    for diagnostic in store.iter_mut() {
+        diagnostic.is_enabled = false;
+    }
 }
 
 fn setup_panel(
+    store: Res<DiagnosticsStore>,
     mut commands: Commands,
 ) {
-    const LABELS: [&'static str; 1] = [ "fps", ];
-    const UNITS: [&'static str; 1] = [ "Hz", ];
-
-    let labels = LABELS.map(|label| commands.spawn(UiText::new_bundle(label)).id());
-    let units = UNITS.map(|label| commands.spawn(UiText::new_bundle(label)).id());
-
-    fn format<T>(property: Property, value: T) -> (Property, String)
-    where
-        T: std::fmt::Display
-    {
-        let value = property.format(value);
-        (property, value)
-    }
-
-    let values = [
-        format(Property::Fps, 0.0),
-    ];
-    let values = values.map(
-        |(property, value)| commands.spawn((
-            UiText::new_bundle(&value),
-            property,
-        )).id()
-    );
-
-    let columns = [labels, values, units];
-    let columns = columns.map(|column| {
-        let mut entity = commands.spawn(
-            Node {
-                display: Display::Flex,
-                flex_direction: FlexDirection::Column,
-                padding: UiRect::all(Val::Px(4.0)),
-                ..default()
-            }
-        );
-        entity.add_children(&column);
-        entity.id()
-    });
-
-    let mut content = commands.spawn(
-        Node {
-            display: Display::Flex,
-            flex_direction: FlexDirection::Row,
-            ..default()
-        },
-    );
-    content.add_children(&columns);
-    let content = content.id();
-
-    let mut panel = UiWindow::new("Statistics", super::WindowLocation::BottomLeft, &mut commands);
-    panel.add_child(content);
-    panel.insert(UiRoot);
+    Property::spawn_panel(&store, &mut commands);
 }
 
 fn update_text(
-    diagnostic: Res<DiagnosticsStore>,
+    store: Res<DiagnosticsStore>,
     query: Query<(Entity, &Property)>,
     mut writer: TextUiWriter,
     time: Res<Time>,
@@ -89,23 +71,124 @@ fn update_text(
     if *time_since_rerender >= Duration::from_millis(100) {
         *time_since_rerender = Duration::ZERO;
         for (entity, property) in query {
-            match property {
-                Property::Fps => {
-                    if let Some(fps) = diagnostic.get(&FrameTimeDiagnosticsPlugin::FPS)
-                        && let Some(value) = fps.smoothed()
-                    {
-                        *writer.text(entity, 0) = property.format(value);
-                    }
-                },
-            }
+            let value = property.get(&store);
+            *writer.text(entity, 0) = property.format(value);
         }
     }
 }
 
+fn toggle_stats (
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut store: ResMut<DiagnosticsStore>,
+    current_state: Res<State<StatsState>>,
+    mut next_state: ResMut<NextState<StatsState>>,
+    panel: Query<Entity, With<StatsPanel>>,
+    mut commands: Commands,
+) {
+    if !keyboard_input.just_pressed(KeyCode::F4) { return }
+    let paths = [
+        &SystemInformationDiagnosticsPlugin::PROCESS_CPU_USAGE,
+        &SystemInformationDiagnosticsPlugin::PROCESS_MEM_USAGE,
+        &FrameTimeDiagnosticsPlugin::FPS,
+    ];
+    match **current_state {
+        StatsState::Disabled => {
+            for path in paths {
+                if let Some(diagnostic) = store.get_mut(path) {
+                    diagnostic.is_enabled = true;
+                }
+            }
+            next_state.set(StatsState::Enabled);
+            Property::spawn_panel(&store, &mut commands);
+        },
+        StatsState::Enabled => {
+            for path in paths {
+                if let Some(diagnostic) = store.get_mut(path) {
+                    diagnostic.is_enabled = false;
+                }
+            }
+            next_state.set(StatsState::Disabled);
+            if let Ok(panel) = panel.single() {
+                commands.entity(panel).despawn();
+            }
+        },
+    }
+}
+
 impl Property {
-    fn format<T: std::fmt::Display>(&self, value: T) -> String {
-        match self {
-            Self::Fps => format!("{:7.0}", value),
+    fn format<T: std::fmt::Display>(&self, value: Option<T>) -> String {
+        match value {
+            Some(value) => match self {
+                Self::Cpu => format!("{:7.0}", value),
+                Self::Fps => format!("{:7.0}", value),
+                Self::Memory => format!("{:7.0}", value),
+            },
+            None => format!("{:7.7}", ""),
         }
+    }
+
+    fn get(&self, store: &DiagnosticsStore) -> Option<f64> {
+        match self {
+            Property::Cpu => store.get(&SystemInformationDiagnosticsPlugin::PROCESS_CPU_USAGE)
+                .and_then(|diagnostic| diagnostic.value()),
+            Property::Fps => store.get(&FrameTimeDiagnosticsPlugin::FPS)
+                .and_then(|diagnostic| diagnostic.smoothed()),
+            Property::Memory => store.get(&SystemInformationDiagnosticsPlugin::PROCESS_MEM_USAGE)
+                .and_then(|diagnostic| diagnostic.value()),
+        }
+    }
+
+    fn spawn_panel(
+        store: &DiagnosticsStore,
+        commands: &mut Commands,
+    ) {
+        const LABELS: [&'static str; 3] = [ "cpu", "memory", "fps", ];
+        const UNITS: [&'static str; 3] = [ " %", " %", "Hz", ];
+
+        let labels = LABELS.map(|label| commands.spawn(UiText::new_bundle(label)).id());
+        let units = UNITS.map(|label| commands.spawn(UiText::new_bundle(label)).id());
+
+        let values = [
+            Property::Cpu,
+            Property::Memory,
+            Property::Fps,
+        ];
+        let values = values.map(|property| {
+            let value = property.get(&store);
+            let value = property.format(value);
+            commands.spawn((
+                UiText::new_bundle(&value),
+                property,
+            )).id()
+        });
+
+        let columns = [labels, values, units];
+        let columns = columns.map(|column| {
+            let mut entity = commands.spawn(
+                Node {
+                    display: Display::Flex,
+                    flex_direction: FlexDirection::Column,
+                    padding: UiRect::all(Val::Px(4.0)),
+                    ..default()
+                }
+            );
+            entity.add_children(&column);
+            entity.id()
+        });
+
+        let mut content = commands.spawn(
+            Node {
+                display: Display::Flex,
+                flex_direction: FlexDirection::Row,
+                ..default()
+            },
+        );
+        content.add_children(&columns);
+        let content = content.id();
+
+        let mut panel = UiWindow::new("Statistics", super::WindowLocation::BottomLeft, commands);
+        panel.add_child(content);
+        panel.insert(StatsPanel);
+        panel.insert(UiRoot);
     }
 }
